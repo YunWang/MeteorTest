bb7cea22017b9ebf7e8e5ee11bdccb22cf120876
YunWang <ivanwangyun@163.com>
2017-04-14 15:58:43 +0800

    Practice according to Meteor全栈开发

---
+++ client/global_helper.js
@@ -0,0 +1,6 @@
+/**
+This file records all global helper.
+*/
+Template.registerHelper('equal',(a,b)=>{
+	return a === b;
+});
--- client/main.js
+++ client/main.js
@@ -3,20 +3,59 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+// Template.hello.onCreated(function helloOnCreated() {
+//   // counter starts at 0
+//   this.counter = new ReactiveVar(0);
+// });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+// Template.hello.helpers({
+//   counter() {
+//     return Template.instance().counter.get();
+//   }
+// });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+
+// Template.hello.events({
+//   'click button'(event, instance) {
+//     // increment the counter when button is clicked
+//     instance.counter.set(instance.counter.get() + 1);
+//   },
+// });
+//----------------------------------------------------------------------------
+// Template.globalHelperExample.helpers({
+// 	foods:function(){
+// 		return [{
+// 			selected:"no",
+// 			name:"烤肉"
+// 		},
+// 		{
+// 			selected:"yes",
+// 			name:"烤鸭"
+// 		},
+// 		{
+// 			selected:"no",
+// 			name:"火锅"
+// 		}];
+// 	}
+// });
+//----------------------------------------------------------------------------
+// Template.outside.events({
+// 	'click button' : function(event, template){
+// 		alert('outside');
+// 		$('body').css('background-color','red');
+// 	}
+// });
+
+// Template.inside.events({
+// 	'click button' : function(event, template){
+// 		alert('inside');
+// 		event.stopImmediatePropagation();
+// 		$('body').css('background-color','blue');
+// 	}
+// });
+//-----------------------------------------------------------
+Template.linktest.events({
+	'click a':function(event,template){
+		event.preventDefault();
+	}
+})
